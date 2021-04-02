6a183f6f1fcd6add628f4937deb8f6c0d5d8447b
Nathan Fisher <nfisher@junctionbox.ca>
2021-04-02 17:14:29 -0400

    Switch to kube-router dont feel like fighting with networking today

--- networking/kube-router.sh
+++ networking/kube-router.sh
@@ -1,4 +1,9 @@
 # install kube-router as k8s network
 kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter-all-features.yaml
 kubectl -n kube-system delete ds kube-proxy
-docker run --privileged -v /lib/modules:/lib/modules --net=host k8s.gcr.io/kube-proxy-amd64:v1.15.1 kube-proxy --cleanup || echo "Done cleanup"
+ctr image pull k8s.gcr.io/kube-proxy-amd64:v1.15.1
+ctr run --privileged --mount type=bind,src=/lib/modules,dst=/lib/modules,options=rbind:ro --net-host k8s.gcr.io/kube-proxy-amd64:v1.15.1 kube-proxy kube-proxy --cleanup || echo "Done cleanup"
+ctr c rm kube-proxy
+
+# ctr run --privileged --mount type=bind,src=/lib/modules,dst=/lib/modules --net-host k8s.gcr.io/kube-proxy-amd64:v1.15.1 kube-proxy --cleanup || echo "Done cleanup"
+# ctr run --privileged -v /lib/modules:/lib/modules --net=host k8s.gcr.io/kube-proxy-amd64:v1.15.1 kube-proxy --cleanup || echo "Done cleanup"
