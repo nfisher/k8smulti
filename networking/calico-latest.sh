562ba8bff26d1ab2828e2df122edcfd4355b54d9
Nathan Fisher <nfisher@junctionbox.ca>
2023-03-01 15:54:16 -0500

    Fix reference to Calico yaml for calico-latest.sh

--- networking/calico-latest.sh
+++ networking/calico-latest.sh
@@ -3,6 +3,6 @@
 export DATASTORE_TYPE=kubernetes
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
-curl https://projectcalico.docs.tigera.io/manifests/calico.yaml -O
+curl https://raw.githubusercontent.com/projectcalico/calico/master/manifests/calico.yaml -O
 kubectl apply -f calico.yaml
 
